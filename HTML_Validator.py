03594cbf621159dea78498e9e2e81e629663254e
buffeinstein <ambikatiwari999@gmail.com>
2023-02-07 22:04:02 -0800

    changed code, should pass both EC and reg

--- HTML_Validator.py
+++ HTML_Validator.py
@@ -1,10 +1,9 @@
 #!/bin/python3
+import re
 
 
 def validate_html(html):
     '''
-    This function performs a limited version of html validation by checking whether every opening tag has a corresponding closing tag.
-
     >>> validate_html('<strong>example</strong>')
     True
     >>> validate_html('<strong>example')
@@ -13,46 +12,26 @@ def validate_html(html):
 
     tags = _extract_tags(html)
     stack = []
-    if len (tags) == 0: 
-        return True 
-    if len (tags) == 1: 
+    if len(tags) <= 1 and len(html) > 0:
         return False
-    else: 
-        for i in range(len(tags)): 
-            if tags[i][1] != '/': #not a closing tag
-                stack.append(tags[i]) #add it 
-            else:  #a closing tag
+    else:
+        for i in range(len(tags)):
+            if tags[i][1] != '/':
+                stack.append(tags[i])
+            else:
                 if len(stack) == 0:
-                     return False #started with a closing tag - cannot be balanced
+                    return False
                 if tags[i][2:] == stack[-1][1:]:
-                     stack.pop()
+                    stack.pop()
+    print("stack = ", stack)
+    if len(stack) == 0:
+        return True
 
-    if len (stack) == 0: 
-        return True 
-    
-       
-       #for tag in tagslst: 
-        
-
-   # stack = []
-   # for symbol in text:
-   #     if symbol in '([{' :
-   #         stack.append(symbol)
-   #     else:
-   #         if len(stack) == 0:
-   #             return False
-   #         if (stack[-1] == '(' and symbol == ')') or \
-   #            (stack[-1] == '[' and symbol == ']') or \
-   #            (stack[-1] == '{' and symbol == '}'):
-   #             stack.pop()
-   #         else:
-   #             return False
-   # return len(stack) == 0
 
 def _extract_tags(html):
     '''
     >>> _extract_tags('Python <strong>rocks</strong>!')
     ['<strong>', '</strong>']
     '''
-    import re
-    return re.findall(r'<.*?>', html)
+    tags = re.findall('<([^ >]+)', html)
+    return list(map(lambda tag: "<" + tag + ">", tags))
